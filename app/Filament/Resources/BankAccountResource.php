c2fb52da2f9bb5eda4f0e0d9f5348c4b1041747c
Ryan Silva <rdsp1@aluno.ifnmg.edu.br>
2023-11-16 13:33:22 -0300

    Criando resource de contas bancárias

---
+++ app/Filament/Resources/BankAccountResource.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\BankAccountResource\Pages;
+use App\Filament\Resources\BankAccountResource\RelationManagers;
+use App\Models\BankAccount;
+use Filament\Forms;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class BankAccountResource extends Resource
+{
+    protected static ?string $model = BankAccount::class;
+
+    protected static ?string $navigationGroup = 'Vendas';
+
+    protected static ?string $navigationLabel = 'Conta Bancária';
+
+    protected static ?string $navigationIcon = 'heroicon-o-currency-dollar';
+
+    protected static ?int $navigationSort = 2;
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')
+                    ->required()
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')->searchable()->sortable()
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListBankAccounts::route('/'),
+            'create' => Pages\CreateBankAccount::route('/create'),
+            'edit' => Pages\EditBankAccount::route('/{record}/edit'),
+        ];
+    }
+}
