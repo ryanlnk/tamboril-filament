e661ef98ff1a57040534cfe27ea2246b02ed369e
Ryan Silva <rdsp1@aluno.ifnmg.edu.br>
2023-11-16 13:35:29 -0300

    Criando resource de vendedores

---
+++ app/Filament/Resources/SellerResource.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\SellerResource\Pages;
+use App\Filament\Resources\SellerResource\RelationManagers;
+use App\Models\Seller;
+use Filament\Forms;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class SellerResource extends Resource
+{
+    protected static ?string $model = Seller::class;
+
+    protected static ?string $navigationGroup = 'Usuários';
+
+    protected static ?string $navigationLabel = 'Vendedores';
+
+    protected static ?string $navigationIcon = 'heroicon-o-user';
+
+    protected static ?int $navigationSort = 2;
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')
+                    ->required()
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')->searchable()->sortable()
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListSellers::route('/'),
+            'create' => Pages\CreateSeller::route('/create'),
+            'edit' => Pages\EditSeller::route('/{record}/edit'),
+        ];
+    }
+}
